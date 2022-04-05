96d99904a28425b8e83e1a4b44436695fe434f3a
Ntholi Nkhatho <ntholi@live.com>
2022-04-05 14:29:04 +0200

    Setup project

--- App.js
+++ App.js
@@ -1,29 +1,39 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { Fontisto } from '@expo/vector-icons';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { Fontisto, AntDesign } from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.post}>
-        <View style={styles.header}>
-          <Image
-            style={styles.profilePic}
-            source={require('./assets/profile-pic.jpg')}
-          />
-          <View style={styles.postDetails}>
-            <Text style={styles.author}>Thabo Lebese</Text>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <Text style={styles.date}>Yesterday</Text>
-              <Fontisto name='world-o' size={15} color='#666' />
+        <View style={styles.postBody}>
+          <View style={styles.header}>
+            <Image
+              style={styles.profilePic}
+              source={require('./assets/profile-pic.jpg')}
+            />
+            <View style={styles.postDetails}>
+              <Text style={styles.author}>Thabo Lebese</Text>
+              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Text style={styles.date}>Yesterday</Text>
+                <Fontisto name='world-o' size={15} color='#9E9E9E' />
+              </View>
             </View>
           </View>
+          <View>
+            <Text>
+              Amazing times in This long text that I want to write here
+            </Text>
+            <Image
+              style={styles.postImage}
+              source={require('./assets/ocean.jpg')}
+            />
+          </View>
         </View>
-        <View>
-          <Text>Amazing times in This long text that I want to write here</Text>
-          <Image
-            style={styles.postImage}
-            source={require('./assets/ocean.jpg')}
-          />
+        <View style={styles.footer}>
+          <TouchableOpacity style={styles.footerButton}>
+            <AntDesign name='like1' size={24} color='#9E9E9E' />
+            <Text style={styles.footerText}>Like</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -33,14 +43,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#666',
+    backgroundColor: '#EEEEEE',
     alignItems: 'center',
     justifyContent: 'center',
   },
   post: {
     backgroundColor: '#fff',
+    borderColor: '#E0E0E0',
+    borderWidth: 1,
     width: '80%',
-    height: '50%',
+  },
+  postBody: {
     padding: 10,
   },
   profilePic: {
@@ -58,11 +71,30 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   date: {
-    color: '#666',
+    color: '#9E9E9E',
     marginRight: 5,
+    fontSize: 13,
   },
   postImage: {
     width: '100%',
     height: 200,
+  },
+  footer: {
+    height: 40,
+    paddingLeft: 10,
+    width: '100%',
+    backgroundColor: '#ECEFF1',
+    borderTopWidth: 1,
+    borderTopColor: '#E0E0E0',
+  },
+  footerButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  footerText: {
+    marginLeft: 5,
+    color: '#9E9E9E',
+    fontWeight: 'bold',
   },
 });
